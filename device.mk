b5591e105277d498523dd8a8e0ec0d5315c1c818
Ivan Meler <i_ivan@windowslive.com>
2015-11-02 21:39:08 +0100

    Set qanelements

--- device.mk
+++ device.mk
@@ -85,7 +85,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
     ro.telephony.call_ring.multiple=false \
-    ro.telephony.call_ring.delay=3000
+    ro.telephony.call_ring.delay=3000 \
+    ro.ril.qanelements=5
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
