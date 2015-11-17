1c2c2e29e2258ae91ff2f6842b3d9e47b766b137
Ivan Meler <i_ivan@windowslive.com>
2015-11-17 17:47:57 +0000

    Revert "Shim library for missing symbols in Samsung blobs"
    This reverts commit 02889ce4dbb938be0e02a80b94142dac33a9e743.

--- device.mk
+++ device.mk
@@ -85,10 +85,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
-# Samsung symbols
-PRODUCT_PACKAGES += \
-    libsamsung_symbols
-
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
